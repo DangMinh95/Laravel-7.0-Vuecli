761f790b8bbab9da75b6ffbc47c1e696411f068f
DangMinh95 <minhdt.vietnam@gmail.com>
2021-03-21 17:18:54 +0700

    init project

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# Laravel-7.0-Vuecli
-SPA laravel pet project
+Run 
+composer install
+npm install
+
+cp .env.example .env
+
+run 
+php artisan key:generate
+php artisan jwt:secret
+npm run watch or npm run dev
+php artisan serve
+
---
+++ resources/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <LoginForm></LoginForm>
+  </div>
+</template>
+
+<script>
+import LoginForm from '../components/LoginForm'
+export default {
+  name: 'Home',
+  components: {
+    LoginForm
+  }
+}
+</script>
